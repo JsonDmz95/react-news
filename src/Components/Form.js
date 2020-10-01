68ada24434ac9af0affe0d0b6affb48288786f96
JsonDmz95 <dominguez_js@outlook.com>
2020-10-01 11:35:43 -0600

    Adding header and initializing form component

---
+++ src/Components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Form = () => {
+  return (
+    <div className="row">
+      <div className="col s12 m8 offset-m2">
+        <form>
+          <h2>Find news by category</h2>
+        </form>
+      </div>
+    </div>
+  );
+}
+ 
+export default Form;
